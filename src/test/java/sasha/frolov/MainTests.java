970c3a83359b01f8dd57c4e01cc82596d4d888ed
Sasha <sasha7692@gmail.com>
2019-10-04 21:18:40 +0300

    Update MainTests.java

--- src/test/java/sasha/frolov/MainTests.java
+++ src/test/java/sasha/frolov/MainTests.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class MainTests {
     @ParameterizedTest(name = "{0} + {1} = {2}")
     @CsvSource({
-            "0,    1,   1",
+            "0,    1,   2",
             "1,    2,   3",
             "49,  51, 100",
             "1,  100, 101"
